6e7024064b3e089ea5db301bba347bbf7dbbab35
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2022-02-02 21:38:43 +0530

    Add files via upload

---
+++ Java/Assignment2/Assignment2Q4/src/com/company/MustDeclareClassAbstract.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class AbstractExample{
+    abstract void sayHello();
+
+    //Used abstract method but didnt made class abstract and its show error
+};
+
+
+
+public class MustDeclareClassAbstract {
+    public static void main(String [] args){
+
+    }
+}
